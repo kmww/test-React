a882b9210c76cb3db0378b5b8f365c0992288ef7
kmww <kmw9708@gmail.com>
2022-12-15 15:29:16 +0900

    상태관리 & 이벤트 바인딩

---
+++ 1-3.state_event/src/App.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import Counter from "./components/Counter";
+
+function App() {
+  const [totalCount, setTotalCount] = useState(0);
+
+  return (
+    <div>
+      TotalCount: {totalCount}
+      <Counter
+        onIncrease={() => setTotalCount(totalCount + 1)}
+        onDecrease={() => setTotalCount(totalCount - 1)}
+      />
+      <Counter
+        onIncrease={() => setTotalCount(totalCount + 1)}
+        onDecrease={() => setTotalCount(totalCount - 1)}
+      />
+      <Counter
+        onIncrease={() => setTotalCount(totalCount + 1)}
+        onDecrease={() => setTotalCount(totalCount - 1)}
+      />
+    </div>
+  );
+}
+
+export default App;
