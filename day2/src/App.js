04f6bb8da2b3a0d9c8930ee78fed4b6d2de586e8
kmww <kmw9708@gmail.com>
2022-12-16 15:48:29 +0900

    useCallback

--- day2/src/App.js
+++ day2/src/App.js
@@ -2,8 +2,9 @@ import Box from "./components/Box";
 import Box2 from "./components/Box/Box2";
 import { css } from "@emotion/react";
 import ShowSum from "./components/UseMemo/ShowSum";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import Box3 from "./components/ReactMemo/Box3";
+import Checkbox from "./components/UseCallback/Checkbox";
 
 const style = css`
   color: hotpink;
@@ -18,6 +19,13 @@ const SomeComponent = ({ children }) => (
 function App() {
   const [label, setLabel] = useState("Result");
   const [count, setCount] = useState(0);
+  const [foodOn, setFoodOn] = useState(false);
+  const [clothesOn, setClothesOn] = useState(false);
+  const [shelterOn, setShelterOn] = useState(false);
+
+  const foodChange = useCallback((e) => setFoodOn(e.target.checked), []);
+  const clothesChange = useCallback((e) => setClothesOn(e.target.checked), []);
+  const shelterChange = useCallback((e) => setShelterOn(e.target.checked), []);
 
   return (
     <div>
@@ -33,6 +41,10 @@ function App() {
       {count}
       <button onClick={() => setCount(count + 1)}>+</button>
       <Box3 />
+      {/* UseCallback */}
+      <Checkbox label="Food" on={foodOn} onChange={foodChange} />
+      <Checkbox label="Clothes" on={clothesOn} onChange={clothesChange} />
+      <Checkbox label="Shelter" on={shelterOn} onChange={shelterChange} />
     </div>
   );
 }
