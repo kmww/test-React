24f91ceac07aca93a6eb21b4bfeca6ea0d828576
kmww <kmw9708@gmail.com>
2022-12-16 14:37:21 +0900

    useMemo

--- day2/src/App.js
+++ day2/src/App.js
@@ -1,6 +1,8 @@
 import Box from "./components/Box";
 import Box2 from "./components/Box/Box2";
 import { css } from "@emotion/react";
+import ShowSum from "./components/UseMemo/ShowSum";
+import { useState } from "react";
 
 const style = css`
   color: hotpink;
@@ -13,12 +15,17 @@ const SomeComponent = ({ children }) => (
 );
 
 function App() {
+  const [label, setLabel] = useState("Result");
+
   return (
     <div>
       <SomeComponent />
       <Box bgColor={"red"} />
       <Box2 />
       <div css={{ width: 100, height: 100, backgroundColor: "black" }} />
+      {/* useMemo */}
+      <button onClick={() => setLabel(label + ":")}>Change Label</button>
+      <ShowSum label={label} n={1000000000} />
     </div>
   );
 }
