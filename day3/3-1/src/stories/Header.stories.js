732506dc008043057eaea0886f4735bec6fc0019
kmww <kmw9708@gmail.com>
2022-12-19 14:34:07 +0900

    component-Header

---
+++ day3/3-1/src/stories/Header.stories.js
@@ -0,0 +1,16 @@
+import Header from "../components/Header";
+
+export default {
+  title: "component/Header",
+  component: Header,
+  argTypes: {
+    level: { control: { type: "range", min: 1, max: 6 } },
+    strong: { control: "boolean" },
+    underline: { control: "boolean" },
+    color: { control: "color" },
+  },
+};
+
+export const Default = (args) => {
+  return <Header {...args}>Header</Header>;
+};
