88d896631eb60d78c4a499722c69878c95627b3c
kmww <kmw9708@gmail.com>
2022-12-20 14:29:28 +0900

    component-Drag&Drop

---
+++ day3/3-1/src/components/Upload/index.js
@@ -0,0 +1,103 @@
+import styled from "@emotion/styled";
+import { useRef, useState } from "react";
+
+const UploadContainer = styled.div`
+  display: inline-block;
+  cursor: pointer;
+`;
+
+const Input = styled.input`
+  display: none;
+`;
+
+const Upload = ({
+  children,
+  droppable,
+  name,
+  accept,
+  value,
+  onChange,
+  ...props
+}) => {
+  const [file, setFile] = useState(value);
+  const [dragging, setDragging] = useState(false);
+
+  const inputRef = useRef(null);
+  const handleFileChange = (e) => {
+    const files = e.target.files;
+    const changedFile = files[0];
+    setFile(changedFile);
+    console.log("enter");
+    onChange && onChange(changedFile);
+  };
+
+  const handleChooseFile = () => {
+    inputRef.current.click();
+  };
+
+  const handleDragEnter = (e) => {
+    console.log("enter");
+    if (!droppable) return;
+
+    e.preventDefault();
+    e.stopPropagation(); //부모나 자식 컴포넌트로 인벤트 전파 막는다.
+
+    if (e.dataTransfer.items && e.dataTransfer.items.length > 0) {
+      setDragging(true);
+    }
+  };
+
+  const handleDragLeave = (e) => {
+    console.log("enter");
+    if (!droppable) return;
+
+    e.preventDefault();
+    e.stopPropagation();
+
+    setDragging(false);
+  };
+
+  const handleDragOver = (e) => {
+    console.log("enter");
+    if (!droppable) return;
+
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
+  const handleFileDrop = (e) => {
+    console.log("enter");
+    if (!droppable) return;
+
+    e.preventDefault();
+    e.stopPropagation();
+
+    const files = e.dataTransfer.files;
+    const changedFile = files[0];
+    setFile(changedFile);
+    onChange && onChange(changedFile);
+    setDragging(false);
+  };
+
+  return (
+    <UploadContainer
+      onClick={handleChooseFile}
+      onDrop={handleFileDrop}
+      onDragEnter={handleDragEnter}
+      onDragLeave={handleDragLeave}
+      onDragOver={handleDragOver}
+      {...props}
+    >
+      <Input
+        ref={inputRef}
+        type="file"
+        name={name}
+        accept={accept}
+        onChange={handleFileChange}
+      />
+      {typeof children === "function" ? children(file, dragging) : children}
+    </UploadContainer>
+  );
+};
+
+export default Upload;
