a061aa96afebd95cfa3e96f275e5bdd7573c8989
kmww <kmw9708@gmail.com>
2022-12-26 15:51:34 +0900

    post(ft.axios)

--- day3/3-1/src/App.js
+++ day3/3-1/src/App.js
@@ -1,5 +1,29 @@
+import axios from "axios";
+import useAsync from "./hooks/useAsync";
+import Header from "./components/Header";
+import Spinner from "./components/Spinner";
+
 function App() {
-  return <div></div>;
+  const initialPosts = useAsync(async () => {
+    return await axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.data);
+  }, []);
+
+  return (
+    <div>
+      <Header>Posts</Header>
+      <ul>
+        {initialPosts.isLoading ? (
+          <Spinner />
+        ) : (
+          (initialPosts.value || []).map((post) => (
+            <li key={post.id}>{post.title}</li>
+          ))
+        )}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
