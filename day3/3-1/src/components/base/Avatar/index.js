ee8872ca5fd3fa0716116133b289a66cfecbc0c4
kmww <kmw9708@gmail.com>
2022-12-26 16:47:29 +0900

    axios 사용하기

--- day3/3-1/src/components/base/Avatar/index.js
+++ day3/3-1/src/components/base/Avatar/index.js
@@ -1,4 +1,4 @@
-import ImageComponent from "../../Image";
+import ImageComponent from "../Image";
 import styled from "@emotion/styled";
 import { useEffect, useState } from "react";
 import AvatarGroup from "./AvatarGroup";
--- day3/3-1/src/components/index.js
+++ day3/3-1/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Avatar } from "./base/Avatar";
-export { default as Header } from "./base/Avatar";
-export { default as Spinner } from "./base/Avatar";
+export { default as Header } from "./base/Header";
+export { default as Spinner } from "./base/Spinner";
+export { default as Text } from "./base/Text/Text";
