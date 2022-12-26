56489093d8d76c6e8ae2b0c24cc3c4d8ce10a68f
kmww <kmw9708@gmail.com>
2022-12-26 14:45:58 +0900

    Feat: "Draw Plugin"

--- ex/[ex4]drawingBoard/src/components/Paint/index.js
+++ ex/[ex4]drawingBoard/src/components/Paint/index.js
@@ -1,4 +1,14 @@
-import { useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { PenPlugin } from "./plugins/pen";
+
+const calculateCoord = (e, canvas) => {
+  const rect = canvas.getBoundingClientRect();
+
+  return {
+    x: e.pageX - rect.left - window.scrollX,
+    y: e.pageY - rect.top - window.scrollY,
+  };
+};
 
 const Paint = ({
   command = "pen",
@@ -6,9 +16,15 @@ const Paint = ({
   lineWidth = 2,
   width = 800,
   height = 600,
+  plugins = [new PenPlugin()],
   style,
   className,
 }) => {
+  const [currentCommand, setCurrentCommand] = useState(command);
+  const [currentLineWidth, setCurrentLineWidth] = useState(lineWidth);
+  const [currentColor, setCurrentColor] = useState(color);
+  const [currentPlugins, setCurrentPlugins] = useState({});
+  const [drawing, setDrawing] = useState(false);
   const canvasRef = useRef();
 
   const scale = typeof window === "undefined" ? 1 : window.devicePixelRatio;
@@ -30,11 +46,107 @@ const Paint = ({
     canvasRef.current.height = height * scale;
 
     canvasRef.current.getContext("2d").scale(scale, scale);
+
+    plugins.forEach((plugin) => {
+      plugin.canvas = canvasRef.current;
+    });
+
+    setCurrentPlugins(
+      Object.assign(
+        {},
+        ...plugins.map((plugin) => ({
+          [plugin.name]: plugin,
+        }))
+      )
+    );
   }, [canvasRef.current, scale]);
+
+  const handleDrawStart = useCallback(
+    (e) => {
+      e.preventDefault();
+
+      const { x, y } = calculateCoord(e, canvasRef.current);
+
+      currentPlugins[currentCommand].draw({
+        x,
+        y,
+        width,
+        height,
+        scale,
+        lineWidth: currentLineWidth,
+        color: currentColor,
+        state: "draw-started",
+      });
+
+      setDrawing(true);
+    },
+    [canvasRef, currentCommand, currentColor, currentLineWidth, currentPlugins]
+  );
+
+  const handleDrawing = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (!drawing) return;
+
+      const { x, y } = calculateCoord(e, canvasRef.current);
+
+      currentPlugins[currentCommand].draw({
+        x,
+        y,
+        width,
+        height,
+        scale,
+        lineWidth: currentLineWidth,
+        color: currentColor,
+        state: "drawing",
+      });
+    },
+    [
+      canvasRef,
+      currentCommand,
+      currentColor,
+      currentLineWidth,
+      currentPlugins,
+      drawing,
+    ]
+  );
+
+  const handleDrawFinish = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (!drawing) return;
+
+      const { x, y } = calculateCoord(e, canvasRef.current);
+
+      currentPlugins[currentCommand].draw({
+        x,
+        y,
+        width,
+        height,
+        scale,
+        lineWidth: currentLineWidth,
+        color: currentColor,
+        state: "draw-finished",
+      });
+
+      setDrawing(false);
+    },
+    [
+      canvasRef,
+      currentCommand,
+      currentColor,
+      currentLineWidth,
+      currentPlugins,
+      drawing,
+    ]
+  );
 
   return (
     <canvas
       ref={canvasRef}
+      onMouseDown={handleDrawStart}
+      onMouseMove={handleDrawing}
+      onMouseUp={handleDrawFinish}
       style={{ ...canvasDefaultStyle, ...canvasSizeStyle, ...style }}
       className={className}
     />
