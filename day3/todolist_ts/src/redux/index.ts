84e74ebebfe173b4824be1d3f6e483a957b9d860
kmww <kmw9708@gmail.com>
2022-12-28 14:12:32 +0900

    refactor: redux-persist 이용해서 storage이용

--- day3/todolist_ts/src/redux/index.ts
+++ day3/todolist_ts/src/redux/index.ts
@@ -2,12 +2,24 @@ import { tasks } from "./tasks";
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import logger from "redux-logger";
 import { composeWithDevTools } from "redux-devtools-extension";
+import session from "redux-persist/lib/storage/session";
+import { persistReducer, persistStore } from "redux-persist";
 
-const rootReducer = combineReducers({ tasks });
+const persistConfig = {
+  key: "root",
+  storage: session,
+  whitelist: ["tasks"],
+};
+
+const combinedReducer = combineReducers({ tasks });
+
+const rootReducer = persistReducer(persistConfig, combinedReducer);
 
 export const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(logger))
 );
+
+export const persistor = persistStore(store as any);
 
 export type RootState = ReturnType<typeof rootReducer>;
