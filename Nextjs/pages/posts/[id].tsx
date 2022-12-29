b8c4b09d2881df2900b253896a51d59afca57adb
kmww <kmw9708@gmail.com>
2022-12-29 15:53:03 +0900

    nextjs data fetching

--- Nextjs/pages/posts/[id].tsx
+++ Nextjs/pages/posts/[id].tsx
@@ -1,13 +1,38 @@
-import { useRouter } from "next/router";
+import { NextPageContext } from "next";
+import { Post } from "../../interfaces";
+import axios from "axios";
 
-const Posts = () => {
-  const router = useRouter();
+export const getServerSideProps = async (context: NextPageContext) => {
+  const postId = context.query.id;
+  try {
+    const { data: post } = await axios(
+      `https://jsonplaceholder.typicode.com/posts/${postId}`
+    );
+    return {
+      props: { post },
+    };
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response?.status === 404) {
+      return {
+        notFound: true,
+      };
+    }
 
+    return { props: {} };
+  }
+};
+
+interface Props {
+  post: Post;
+}
+
+const PostPage = ({ post }: Props) => {
   return (
     <div>
-      <button onClick={() => router.push("/about")}>About</button>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
     </div>
   );
 };
 
-export default Posts;
+export default PostPage;
